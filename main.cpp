01a877c110141873a06a2cecbeea63e1056f6d33
Deekshith0625 <miyapuramdeekshith@gmail.com>
2025-04-22 22:33:44 +0530

    adding class

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include<iostream>
+#include<string>
 using namespace std;
+class chatQueue{
+    private:
+        string username;
+};
 
 int main(){
     cout<<"DS project"<<endl;
-    cout<<"CBIT"<<endl;
 }
