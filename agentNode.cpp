c8cd9e2b056cc996e1b3296d7e0202824a5073d9
rashmith321 <rashmithsheela321@gmail.com>
2025-04-26 00:35:07 +0530

    updated agent

--- agentNode.cpp
+++ agentNode.cpp
@@ -1,12 +1,26 @@
 #include<iostream>
 using namespace std;
 class Agent{
-    private:
-        int ID;
-        node* next;
     public:
+        int ID;
+        bool isAvailable;
         Agent(int id){
             ID = id;
-            next = NULL;
+            isAvailable = true;
          }
 };
+
+void releaseAgent(vector<Agent>& agents, int agentId) {
+    for (auto& agent : agents) {
+        if (agent.ID == agentId) {
+            if (!agent.isAvailable) {
+                agent.isAvailable = true;
+               cout << "Agent " << agent.ID << " is now available.\n";
+            } else {
+                cout << "Agent " << agent.ID << " is already available.\n";
+            }
+            return;
+        }
+    }
+    cout << "Agent ID not found.\n";
+}
