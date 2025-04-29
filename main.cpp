5131f8838cadeafacf5ad1cfa7c9a94db28715bb
Deekshith0625 <miyapuramdeekshith@gmail.com>
2025-04-29 17:46:43 +0530

    changes

--- main.cpp
+++ main.cpp
@@ -187,8 +187,8 @@ int main() {
         cout << "4. Show queue\n";
          cout<<"5.show serving list\n";
         cout<<"6.Release user\n";
-        cout<<"7.EXIT";
-       cout << "Enter choice: ";
+        cout<<"7.EXIT"<<endl;
+       cout <<"Enter choice: "<<endl;
        
       cin >> choice;
 
